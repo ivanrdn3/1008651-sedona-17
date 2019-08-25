7e38721c6feb274af70718cee71de34e40cfc8ca
Ivan <ivan.rdn3@yandex.ru>
2019-08-25 22:42:30 +1000

    Защита проекта

---
+++ source/js/js.js
@@ -0,0 +1,14 @@
+var navMain = document.querySelector(".page-header__nav");
+var navToggle = document.querySelector(".page-header__toggle");
+
+  navMain.classList.remove("page-header__nav--nojs");
+
+  navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("page-header__nav--closed")) {
+    navMain.classList.remove("page-header__nav--closed");
+    navMain.classList.add("page-header__nav--opened");
+  } else {
+    navMain.classList.add("page-header__nav--closed");
+    navMain.classList.remove("page-header__nav--opened");
+  }
+});
